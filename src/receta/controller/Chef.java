4ff7c3c10398007dc56cb7c0eec8caffccc57227
Evelyn-da <Evelyn-da@192.168.200.34>
2017-02-05 22:35:49 -0500

    TG-13  TG-217 Se creo el metodo de calcular el valor de una receta, la operacion esta bien, pero tengo un problema al hacer la multiplicacion, pues me esta cogiendo las cantidades erroneas :(

--- src/receta/controller/Chef.java
+++ src/receta/controller/Chef.java
@@ -10,12 +10,16 @@ import receta.dto.Receta;
 import receta.dto.def.Ingrediente;
 import receta.dto.def.Solido;
 import receta.dto.Liquido;
+import receta.utilidades.CargaManual;
+import receta.utilidades.Cargador;
+import receta.dto.Receta;
 
 /**
  *
  * @author Evelyn-da
  */
 public class Chef {
+    
     public void cocinarReceta(Cocina objCocina, Receta objReceta)
     {
         Ingrediente [] ingredientesReceta = objReceta.getIngrediente();
@@ -24,9 +28,26 @@ public class Chef {
         for (int i = 0; i < ingredientesReceta.length; i++){
             for (int j = 0; j < ingredientesCocina.length; j++){
                 if (ingredientesReceta[i].getNombre().equals(ingredientesCocina[j].getNombre()))
+                    System.out.println("i"+ingredientesReceta[i].getNombre());
+                    System.out.println("j"+ingredientesCocina[j].getNombre());
                     ingredientesReceta[i].usarIngrediente(ingredientesCocina[j]);
-                    System.out.println("se uso ingrediente");
             }
         }
+    }
+    
+    public double valorReceta(Receta[] recetas,String nombreReceta){
+        double valor1 = 0;
+        double valor2 = 1;
+        for (int i = 0; i < recetas.length; i++){
+            System.out.println("nombre " + recetas[i].getNombre());
+            if(recetas[i].getNombre().equals(nombreReceta)){
+                for (int j = 0; j < recetas[i].getIngrediente().length;j++){
+                    valor1 += recetas[i].getIngrediente()[j].getValor()* recetas[i].getIngrediente()[j].getCantidad(); //No logro capturar la cantidad real, ideas?
+                    System.out.println("valorA2 "+valor1+" "+valor2 + " " + recetas[i].getIngrediente()[j].getNombre());
+                }
+            }
+        }
+        System.out.println("valor2 "+valor1);
+        return valor1;
     }
 }
--- src/receta/dto/Liquido.java
+++ src/receta/dto/Liquido.java
@@ -20,15 +20,23 @@ public class Liquido extends Ingrediente{
     
     @Override
     public void usarIngrediente(Ingrediente ingrediente){
+        int x;
         //se va oviar la conversion de medidas queda pendiente para una futura version
         //Por ejempo si se necesita one cup etc
         if(this.getCantidad() > ingrediente.getCantidad()){ 
+            System.out.println("Cantidad1 " + this.getCantidad());
+            System.out.println("Cantidad2 " + ingrediente.getCantidad());
             if(this.getMedida().equals(ingrediente.getMedida())){
-                this.setCantidad(this.getCantidad()- ingrediente.getCantidad());
+                System.out.println("Medida1 " + this.getMedida());
+                System.out.println("Medida2 " + ingrediente.getMedida());
+                x=this.getCantidad() - ingrediente.getCantidad();
+                this.setCantidad(this.getCantidad() - ingrediente.getCantidad());
+                System.out.println("x"+x+"cantidad nueva"+this.getCantidad());
+                System.out.println("nombre"+this.getNombre());
             }else{
-                System.out.println("se debe hacer una conversion");
+                System.out.println("se debe hacer una conversion-Liquido");
             }
-            System.out.println("no hay inventario suficiente");  
+            //System.out.println("no hay inventario suficiente-Liquido");  
         }
     }
     
